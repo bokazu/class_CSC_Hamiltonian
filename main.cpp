c9c05ec0a1638e36f3c9547250861b7b5a2e6372
bokazu <kazu.u.d.l@outlook.com>
2023-03-04 00:55:41 +0900

    editted main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include "CSC_Hamiltonian/CSC_Hamiltonian.hpp"
-#include "CSC_Sub_Hamiltonian/CSC_Sub_Hamiltonian.hpp"
+// #include "CSC_Sub_Hamiltonian/CSC_Sub_Hamiltonian.hpp"　//各磁化ごとの部分空間における対角化を行う場合はこちらを使用する
 
 using namespace std;
 
 int main()
 {
 
-    /*全状態空間での対角化*/
-    // CSC_Hamiltonian H("model/jset_4site.txt", 4);
-    // H.csc_hamiltonian();
-    // H.csc_lanczos(10);
-    // cout << H << endl;
+    /*-------------------全状態空間での対角化------------------------*/
+    CSC_Hamiltonian H("model/jset_4site.txt", 4); //オブジェクトの構築
+    H.csc_hamiltonian(); //ハミルトニアン行列要素の計算
+    H.csc_lanczos(10); //ランチョス法を用いた数値対角化の実行
+    cout << H << endl; //結果の出力
+    /*-----------------------------------------------------------*/
 
-    /*部分状態空間での対角化*/
-    CSC_Sub_Hamiltonian H("model/jset_4site.txt", 4, 2);
-    H.subspace_check();
-    H.csc_sub_hamiltonian();
+    /*------------各磁化ごとの部分空間における対角化を行う場合にはこちらを使用する------------*/
+    // CSC_Sub_Hamiltonian H("model/jset12site_radder.txt", 12, 11);
+    // H.subspace_check();
+    // H.csc_sub_hamiltonian();
     // for (int i = 0; i < 6; i++)
     // {
     //     cout << "bm[" << i << "] = " << H.bm[i] << endl;
     //     cout << "gbm[bm[" << i << " ]] = " << H.gbm[H.bm[i]] << endl;
     // }
+    /*-------------------------------------------------------------------------*/
 
     // for (int i = 0; i < H.nnz; i++)
     // {
@@ -31,6 +33,6 @@ int main()
     // {
     //     cout << "col_ptr[" << i << "] = " << H.col_ptr[i] << endl;
     // }
-    H.csc_sub_lanczos(20, 'N', 'y');
-    cout << H << endl;
+    // H.csc_sub_lanczos(100, 'V');
+    // cout << H << endl;
 }
